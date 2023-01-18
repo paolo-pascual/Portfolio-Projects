c1f9437ea710dae0243fa842cad1d80a9f749964
Paolo Pascual <122790242+paolo-pascual@users.noreply.github.com>
2023-01-18 10:16:08 +0800

    Update Covid_Data_Exploration.sql

--- Covid_Data_Exploration.sql
+++ Covid_Data_Exploration.sql
@@ -86,8 +86,8 @@ ORDER BY TotalCasesToPopulationRatio DESC
 
 SELECT location
       ,MAX(CAST(total_deaths AS int)) AS TotalDeaths
-	  ,population
-	  ,MAX(ROUND(CAST(total_deaths AS int)/population, 4)) AS DeathToPopulationRatio
+      ,population
+      ,MAX(ROUND(CAST(total_deaths AS int)/population, 4)) AS DeathToPopulationRatio
 FROM Portfolio..deaths
 WHERE continent IS NOT NULL
 GROUP BY location, population
@@ -97,9 +97,9 @@ ORDER BY DeathToPopulationRatio DESC
 -- Global Total Numbers of Deaths vs Total Covid Cases Daily
 
 SELECT date 
-	  ,SUM(CAST(new_deaths AS int)) AS GlobalTotalDeaths
+      ,SUM(CAST(new_deaths AS int)) AS GlobalTotalDeaths
       ,SUM(new_cases) AS GlobalNewCases 
-	  ,ROUND(SUM(CAST(new_deaths AS int))/SUM(new_cases), 4) AS GlobalTotalDeathsToCasesRatio
+      ,ROUND(SUM(CAST(new_deaths AS int))/SUM(new_cases), 4) AS GlobalTotalDeathsToCasesRatio
 FROM Portfolio..deaths
 WHERE continent IS NOT NULL
 GROUP BY date
@@ -116,12 +116,12 @@ SELECT *
       ,CASE WHEN people_vaccinated IS NOT NULL OR people_vaccinated <> 0 THEN
 	   people_vaccinated -(CASE WHEN 
 	                       MAX(CAST(people_vaccinated AS bigint)) 
-						   OVER (PARTITION BY location ORDER BY date ROWS BETWEEN UNBOUNDED PRECEDING AND 1 PRECEDING) IS NULL
-						   THEN 0
-						   ELSE
-						   MAX(CAST(people_vaccinated AS bigint)) 
-						   OVER (PARTITION BY location ORDER BY date ROWS BETWEEN UNBOUNDED PRECEDING AND 1 PRECEDING) 
-						   END)
+			       OVER (PARTITION BY location ORDER BY date ROWS BETWEEN UNBOUNDED PRECEDING AND 1 PRECEDING) IS NULL
+			       THEN 0
+			       ELSE
+			       MAX(CAST(people_vaccinated AS bigint)) 
+			       OVER (PARTITION BY location ORDER BY date ROWS BETWEEN UNBOUNDED PRECEDING AND 1 PRECEDING) 
+			       END)
        ELSE NULL END AS new_people_vaccinated
 FROM Portfolio..vaccinations
 
@@ -185,13 +185,13 @@ CREATE TABLE VaccRatio
 
 INSERT INTO VaccRatio
 SELECT d.continent 
-	  ,d.location 
-	  ,d.date 
-	  ,d.population 
-	  ,v.new_people_vaccinated
-	  ,SUM(CAST(v.new_people_vaccinated AS bigint)) 
-	       OVER (PARTITION BY d.location ORDER BY d.location, d.date)
-		   AS CumulativeVaccPeopleNumber
+      ,d.location 
+      ,d.date 
+      ,d.population 
+      ,v.new_people_vaccinated
+      ,SUM(CAST(v.new_people_vaccinated AS bigint)) 
+	   OVER (PARTITION BY d.location ORDER BY d.location, d.date)
+	   AS CumulativeVaccPeopleNumber
 FROM Portfolio..deaths d INNER JOIN vaccinations_upd v
 	 ON d.location = v.location AND d.date = v.date
 WHERE d.continent IS NOT NULL
@@ -210,9 +210,9 @@ AS
 SELECT DATEPART(yyyy, d.date)
 	  ,CASE WHEN DATEPART(mm, d.date) BETWEEN 1 AND 3 THEN 1
 	        WHEN DATEPART(mm, d.date) BETWEEN 4 AND 6 THEN 2
-			WHEN DATEPART(mm, d.date) BETWEEN 7 AND 9 THEN 3
-			WHEN DATEPART(mm, d.date) BETWEEN 10 AND 12 THEN 4
-			ELSE NULL END
+		WHEN DATEPART(mm, d.date) BETWEEN 7 AND 9 THEN 3
+		WHEN DATEPART(mm, d.date) BETWEEN 10 AND 12 THEN 4
+		ELSE NULL END
 	  ,CAST(v.new_vaccinations AS bigint)
 	  ,CAST(d.new_deaths AS bigint)
 FROM vaccinations_upd v INNER JOIN Portfolio..deaths d
